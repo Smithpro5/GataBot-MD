9f16b613c1b171d3dd1feeecc404a02946137528
Smithpro5 <106465604+Smithpro5@users.noreply.github.com>
2022-06-02 14:54:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
   ['51917611323', 'Destroyer', true],
-  ['51921909260'],
+  ['51921909260', 'Android', true],
   ['51933462225'],
   ['50061302']
 ]
